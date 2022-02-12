75998e9a3ec1e1c91f7f7c649530b80cbb090c57
tayeful912 <kazitayefulislam12@gmail.com>
2022-02-12 12:43:52 +0600

    file added

---
+++ penguin-fashion-tailwind/app.js
@@ -0,0 +1,10 @@
+document.getElementById('head-buy-btn').addEventListener('click', function () {
+
+
+    let getHeadTitle = document.getElementById('head-title')
+    getHeadTitle.innerText = "I AM PENGUIN";
+
+
+})
+
+// done
